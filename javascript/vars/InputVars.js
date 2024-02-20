afd220114241f92a5377b9fb3078c58289534491
Ben Bingham <bbingham1999@gmail.com>
2024-02-19 19:54:18 -0500

    add var/InputVars.js

---
+++ javascript/vars/InputVars.js
@@ -0,0 +1,30 @@
+inputVars = {
+    numpad:[
+        {inputName:'upleft', inputKey:'Numpad7', movePlayer:true},
+        {inputName:'up', inputKey:'Numpad8', movePlayer:true},
+        {inputName:'upright', inputKey:'Numpad9', movePlayer:true},
+        {inputName:'left', inputKey:'Numpad4', movePlayer:true},
+        {inputName:'wait', inputKey:'Numpad5', wait:true},
+        {inputName:'right', inputKey:'Numpad6', movePlayer:true},
+        {inputName:'downleft', inputKey:'Numpad1', movePlayer:true},
+        {inputName:'down', inputKey:'Numpad2', movePlayer:true},
+        {inputName:'downright', inputKey:'Numpad3', movePlayer:true},
+
+        {inputName:'item1', inputKey:'Digit1', useItem:true},
+        {inputName:'item2', inputKey:'Digit2', useItem:true},
+        {inputName:'item3', inputKey:'Digit3', useItem:true},
+        {inputName:'item4', inputKey:'Digit4', useItem:true},
+        {inputName:'item5', inputKey:'Digit5', useItem:true},
+        {inputName:'item6', inputKey:'Digit6', useItem:true},
+        {inputName:'item7', inputKey:'Digit7', useItem:true},
+        {inputName:'item8', inputKey:'Digit8', useItem:true},
+        {inputName:'item9', inputKey:'Digit9', useItem:true},
+        {inputName:'item10', inputKey:'Digit0', useItem:true},
+
+        {inputName:'drop', inputKey:'KeyD', drop:true},
+        {inputName:'rewind', inputKey:'Backspace', rewind:true}       
+    ],
+    caleb:[
+
+    ]
+}

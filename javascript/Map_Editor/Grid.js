b92b569be1d409a47652ef6e2a742268e29f852f
Ben Bingham <Bbingham1999@gmail.com>
2024-03-20 12:50:28 -0400

    properly init grid

--- javascript/Map_Editor/Grid.js
+++ javascript/Map_Editor/Grid.js
@@ -10,6 +10,17 @@ class Grid{
         Grid.matrix = new Array(Grid.height).fill().map( ()=>
             Array(Grid.width).fill(false)
         )
+
+        $('#map-grid').html('').css('grid-template-columns','repeat('+Grid.width+',1fr)');        
+        for(let y=0; y < Grid.height; y++){
+            for(let x=0; x < Grid.width; x++){
+                $('#map-grid').append(
+                    $('<div>').addClass('map-grid-div').attr('id','map-grid-' + x + '-' + y).append(
+                        $('<div>').addClass('map-entity-div').attr('id','map-entity-' + x + '-' + y)
+                    )
+                )                 
+            }
+        }
     }
 
     static getTile(x,y){
@@ -18,6 +29,10 @@ class Grid{
 
     static setTile(x,y,val){
         Grid.matrix[y][x] = val;
+    }
+
+    static renderGrid(){
+        let entityGroups = EntityGroupManager.EntityGroups;
     }
 
 }

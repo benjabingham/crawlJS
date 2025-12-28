04afadb94a70924768b9dbf975abcb4bd535d3f9
Ben Bingham <Bbingham1999@gmail.com>
2025-12-28 18:32:21 -0500

    indexVars

--- javascript/index/indexVars.js
+++ javascript/index/indexVars.js
@@ -1,4 +1,5 @@
 let mainReleases = [
+    {name: 'v0.4.0', branchName: 'v0.4.0'},
     {name: 'v0.3.2', branchName: 'v0.3.2'},
     {name: 'v0.3.1', branchName: 'v0.3.1'},
     {name: 'v0.3.0', branchName: 'v0.3.0'},
@@ -9,7 +10,10 @@ let mainReleases = [
 ]
 
 let devBranches = [
+    /*
     {name: 'Test', branchName: 'test-0.4.0'},
     {name: 'Dev', branchName: 'dev'},
+
     {name: 'Experimental', branchName: 'experimental'},
+    */
 ]

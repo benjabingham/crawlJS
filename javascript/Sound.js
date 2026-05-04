4259f3a0a800d46932d704ac90973e0a3dbaf6e0
Ben Bingham <Bbingham1999@gmail.com>
2026-05-04 17:58:15 -0400

    sound stuff

--- javascript/Sound.js
+++ javascript/Sound.js
@@ -1,22 +1,33 @@
 class Sound{
-    static hit = new Audio('audio/hit.mp3');
-    static click = new Audio('audio/click.mp3')
-    static die = new Audio('audio/die.mp3')
+    static sounds = {
+        hit:new Audio('audio/hit.mp3'),
+        click:new Audio('audio/click.mp3'),
+        die:new Audio('audio/die.mp3')
+    }
+
+
+    static soundInit(){
+        Object.entries(Sound.sounds).forEach(sound=>{
+            sound.onEnded = ()=>{
+                //TODO - this doesnt work. Need to find a way to reload all sounds once they're done playing.
+                console.log('done')
+            }
+        })
+    }
 
     static playHit(){
 
-        Sound.hit.load();
-        Sound.hit.play();
-
+        Sound.sounds.hit.load();
+        Sound.sounds.hit.play();
     }
 
     static playClick(){
-        Sound.click.load();
-        Sound.click.play();
+        Sound.sounds.click.load();
+        Sound.sounds.click.play();
     }
 
     static playDie(){
-        Sound.die.load();
-        Sound.die.play();
+        Sound.sounds.die.load();
+        Sound.sounds.die.play();
     }
 }
